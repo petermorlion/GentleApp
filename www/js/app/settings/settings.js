72c05c17b6727238e44050d24d29fc547b16d44e
petermorlion <peter.morlion@live.be>
2014-09-12 17:16:05 +0200

    installed ionic

--- www/js/app/settings/settings.js
+++ www/js/app/settings/settings.js
@@ -2,7 +2,7 @@
     'use strict';
     var controllerId = 'settings';
     angular.module('gu')
-        .controller(controllerId, [function() {
+        .controller(controllerId, ['$http', function($http) {
             var vm = this;
             vm.teams = ["Masters", "Open"];
         } ]);
