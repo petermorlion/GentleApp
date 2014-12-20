87a629f20c2ed861c0d6c816110699b85b74b056
petermorlion <peter.morlion@live.be>
2014-12-20 11:24:49 +0100

    removed everything, starting anew

--- www/js/app/settings/settings.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    var controllerId = 'settings';
-    angular.module('gu')
-        .controller(controllerId, ['$http', function($http) {
-            var vm = this;
-            vm.teams = ["Masters", "Open"];
-        } ]);
-})();
