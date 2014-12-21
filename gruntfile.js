47d1bd5a0bbdf8687db5e3408793af05cdcf5f5a
petermorlion <peter.morlion@live.be>
2014-12-21 18:09:28 +0100

    simple test with jasmine and grunt

--- gruntfile.js
+++ gruntfile.js
@@ -2,16 +2,18 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jasmine : {
       // Your project's source files
-      src : 'www/js/**/*.js',
-      // Your Jasmine spec files
-      specs : 'test/**/*Spec.js',
+      src : ['www/js/controllers/**/*.js'],
+      options: {
+        // Your Jasmine spec files
+        specs : 'test/**/*Spec.js',
+      }
       // Your spec helper files
       //helpers : 'specs/helpers/*.js'
     }
   });
 
   // Register tasks.
-  grunt.loadNpmTasks('grunt-jasmine-runner');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   // Default task.
   grunt.registerTask('default', 'jasmine');
---
+++ test/loginControllerSpec.js
@@ -0,0 +1,9 @@
+describe("A loginController", function() {
+  it("should return true", function() {
+    var foo = 0;
+    foo += 1;
+
+    expect(foo).toEqual(1);
+  });
+});
+
