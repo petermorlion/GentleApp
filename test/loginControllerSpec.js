ff26b81925fe0628f8565eea1e67228244a6f53f
petermorlion <peter.morlion@live.be>
2014-12-24 11:34:24 +0100

    tests run via karma, not via gulp yet

--- test/loginControllerSpec.js
+++ test/loginControllerSpec.js
@@ -1,4 +1,14 @@
 describe("A loginController with credentials", function() {
+  var messageService, scope = null;
+
+  beforeEach(function() {
+    scope = {
+      $apply: function() {}
+    };
+
+    spyOn(scope, '$apply');
+  });
+
   it("should login initially", function() {
     var drupalClient = {
       systemConnect: function(success, error) {
@@ -12,10 +22,11 @@ describe("A loginController with credentials", function() {
       }
     };
 
-    var loginController = new LoginController(drupalClient);
+    var loginController = new LoginController(drupalClient, messageService, scope);
 
     expect(loginController.isLoggedIn).toBe(true);
     expect(loginController.isBusy).toBe(false);
+    expect(scope.$apply).toHaveBeenCalled();
   });
 });
 
