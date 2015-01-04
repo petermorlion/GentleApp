9c23de1cf4630a7678293a5112bdfb3f209c0835
petermorlion <peter.morlion@live.be>
2015-01-04 17:45:32 +0100

    redirect to login when nog logged in yet

--- test/appControllerSpec.js
+++ test/appControllerSpec.js
@@ -1,15 +1,60 @@
-describe('An appController, when closing the menu', function() {
-	var messageService, scope = null;
+describe('An appController, when constructing and not logged in', function() {
+	var messageService, scope, drupalClient, sessionData, state = null
 
 	beforeEach(function() {
 		messageService = {};
 		scope = {
 			$on: function() {}
 		};
+
+		sessionData = {
+	    	user: {
+	        	uid: 0
+	      	}
+	    };
+
+		drupalClient = {
+			systemConnect: function(success, error) {
+		        success(sessionData);
+		    }
+		};
+
+		spyOn(drupalClient, 'systemConnect').and.callThrough();
+
+		state = {
+			go: function(to) {}
+		};
+
+		spyOn(state, 'go');
+	});
+
+	it('should redirect to the login page', function() {
+		var appController = new AppController(messageService, scope, drupalClient, state)
+
+		expect(drupalClient.systemConnect).toHaveBeenCalledWith(
+	      	jasmine.any(Function), 
+	      	jasmine.any(Function),
+	      	jasmine.objectContaining({'Content-Type': 'application/json'}));
+
+		expect(state.go).toHaveBeenCalledWith('login');
+	});
+});
+
+describe('An appController, when closing the menu', function() {
+	var messageService, scope, drupalClient = null;
+
+	beforeEach(function() {
+		messageService = {};
+		scope = {
+			$on: function() {}
+		};
+		drupalClient = {
+			systemConnect: function() {}
+		};
 	});
 
 	it('should set the menu as closed', function() {
-		var appController = new AppController(messageService, scope);
+		var appController = new AppController(messageService, scope, drupalClient);
 
 		appController.closeMenu();
 
@@ -18,17 +63,20 @@ describe('An appController, when closing the menu', function() {
 });
 
 describe('An appController, when opening the menu', function() {
-	var messageService, scope = null;
+	var messageService, scope, drupalClient = null;
 
 	beforeEach(function() {
 		messageService = {};
 		scope = {
 			$on: function() {}
 		};
+		drupalClient = {
+			systemConnect: function() {}
+		};
 	});
 
 	it('should set the menu as opened', function() {
-		var appController = new AppController(messageService, scope);
+		var appController = new AppController(messageService, scope, drupalClient);
 
 		appController.openMenu();
 
--- www/js/controllers/appController.js
+++ www/js/controllers/appController.js
@@ -1,5 +1,7 @@
-function AppController(messageService, $scope) {
+function AppController(messageService, $scope, drupalClient, $state) {
 	var vm = this;
+
+	vm._headers = {'Content-Type': 'application/json'};
 
 	vm.isMenuOpen = false;
 
@@ -7,6 +9,18 @@ function AppController(messageService, $scope) {
 		{ sref: 'app.home', label: 'Home' },
 		{ sref: 'app.login', label: 'Inloggen' },
 	];
+
+	drupalClient.systemConnect(function(sessionData) {
+		if (sessionData.user.uid !== 0) {
+			// TODO: test, UI, manual test
+			vm.isBusy = false;
+		}
+		else {
+			$state.go('login');
+		}
+	}, function(err){
+		// TODO: handle error
+	}, vm._headers);
 
 	$scope.$on('loginSuccessful', function(){
 		for (var i = 0; i < vm.menuItems.length; i++) {
