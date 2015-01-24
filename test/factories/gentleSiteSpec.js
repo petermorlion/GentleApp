690e57fc4a7dc0eb251a46a0ce24d91ad6b56e0c
petermorlion <peter.morlion@live.be>
2015-01-24 11:39:40 +0100

    refactored gentleSiteSpec

--- test/factories/gentleSiteSpec.js
+++ test/factories/gentleSiteSpec.js
@@ -1,402 +1,215 @@
-describe('A GentleSite, when creating with session', function() {
-  var gentleSite, drupalClient, sessionData = null;
+describe('A GentleSite', function() {
+  var gentleSite, drupalClient, deferred, $q, $rootScope = null;
 
   beforeEach(function() {
     drupalClient = {
-      systemConnect: function(success, error) {
-        sessionData = {
-          user: { uid: 10 }
-        };
-        success(sessionData);
+      systemConnect: function(success, error) {},
+      login: function(username, password) {},
+      logout: function() {}
+    };
+
+    deferred = {
+      promise: function() {
+        promise = {};
+        return promise;
+      },
+      resolve: function() {},
+      reject: function() {}
+    };
+
+    spyOn(deferred, 'resolve');
+    spyOn(deferred, 'reject');
+
+    $q = {
+      defer: function() {
+        return deferred;
       }
     };
 
-    spyOn(drupalClient, 'systemConnect').and.callThrough();
+    $rootScope = {
+      $apply: function() {}
+    };
 
-    gentleSite = new GentleSite(drupalClient, null);
+    spyOn($rootScope, '$apply');
   });
 
-  it('should call the drupalClient', function() {
-    expect(drupalClient.systemConnect).toHaveBeenCalledWith(
-      jasmine.any(Function),
-      jasmine.any(Function),
-      jasmine.objectContaining({'Content-Type': 'application/json'}));
+  describe('when session exists', function() {
+    beforeEach(function() {
+      drupalClient.systemConnect = function(success, error) {
+        success({
+          user: { uid: 10 }
+        });
+      };
+
+      spyOn(drupalClient, 'systemConnect').and.callThrough();
+
+      gentleSite = new GentleSite(drupalClient, $q, $rootScope);
+    });
+
+    it('should call the drupalClient', function() {
+      expect(drupalClient.systemConnect).toHaveBeenCalledWith(
+        jasmine.any(Function),
+        jasmine.any(Function),
+        jasmine.objectContaining({'Content-Type': 'application/json'}));
+      });
+
+    it('should mark itself as logged in', function() {
+      expect(gentleSite.isLoggedIn).toBe(true);
+    });
+
+    it('should set the uid', function() {
+      expect(gentleSite.uid).toBe(10);
+    });
   });
 
-  it('should mark itself as logged in', function() {
-    expect(gentleSite.isLoggedIn).toBe(true);
-  });
-
-  it('should set the uid', function() {
-    expect(gentleSite.uid).toBe(10);
-  });
-});
-
-describe('A GentleSite, when logging in', function() {
-  var gentleSite, drupalClient, $q, result, promise, userData, deferred, $rootScope = null;
-
-  beforeEach(function() {
-    drupalClient = {
-      systemConnect: function() {},
-      login: function(username, password, success, error) {
+  describe('when logging in', function() {
+    beforeEach(function() {
+      drupalClient.login = function(username, password, success, error) {
         userData = { uid: 10 };
         success(userData);
-      }
-    };
-    spyOn(drupalClient, 'login').and.callThrough();
+      };
 
-    deferred = {
-      promise: function() {
-        promise = {};
-        return promise;
-      },
-      resolve: function() {}
-    };
-    spyOn(deferred, 'resolve');
+      spyOn(drupalClient, 'login').and.callThrough();
 
-    $q = {
-      defer: function() {
-        return deferred;
-      }
-    };
+      gentleSite = new GentleSite(drupalClient, $q, $rootScope);
 
-    $rootScope = {
-      $apply: function() {}
-    };
-    spyOn($rootScope, '$apply');
-
-    gentleSite = new GentleSite(drupalClient, $q, $rootScope);
-
-    result = gentleSite.login('u', 'p');
-  });
-
-  it('should return a Promise', function() {
-    expect(result()).toBe(promise);
-  });
-
-  it('should login via the drupalClient', function() {
-    expect(drupalClient.login).toHaveBeenCalledWith(
-      'u',
-      'p',
-      jasmine.any(Function),
-      jasmine.any(Function),
-      jasmine.objectContaining({'Content-Type': 'application/json'}));
-  });
-
-  it('should have called the resolve function of the deferred', function() {
-    expect(deferred.resolve).toHaveBeenCalledWith(userData);
-  });
-
-  it('should trigger the digest cycle', function() {
-    expect($rootScope.$apply).toHaveBeenCalled();
-  });
-
-  it('should set the uid', function() {
-    expect(gentleSite.uid).toBe(10);
-  });
-});
-
-describe('A GentleSite, when failed to log in', function() {
-  var gentleSite, drupalClient, $q, result, promise, e, deferred, $rootScope = null;
-
-  beforeEach(function() {
-    drupalClient = {
-      systemConnect: function() {},
-      login: function(username, password, success, error) {
-        e = {};
-        error(e);
-      }
-    };
-    spyOn(drupalClient, 'login').and.callThrough();
-
-    deferred = {
-      promise: function() {
-        promise = {};
-        return promise;
-      },
-      reject: function() {}
-    };
-    spyOn(deferred, 'reject');
-
-    $q = {
-      defer: function() {
-        return deferred;
-      }
-    };
-
-    $rootScope = {
-      $apply: function() {}
-    };
-    spyOn($rootScope, '$apply');
-
-    gentleSite = new GentleSite(drupalClient, $q, $rootScope);
-
-    result = gentleSite.login('u', 'p');
-  });
-
-  it('should return a Promise', function() {
-    expect(result()).toBe(promise);
-  });
-
-  it('should tried to login via the drupalClient', function() {
-    expect(drupalClient.login).toHaveBeenCalledWith(
-      'u',
-      'p',
-      jasmine.any(Function),
-      jasmine.any(Function),
-      jasmine.objectContaining({'Content-Type': 'application/json'}));
+      result = gentleSite.login('u', 'p');
     });
 
-  it('should have called the reject function of the deferred', function() {
-    expect(deferred.reject).toHaveBeenCalledWith(e);
+    it('should return a Promise', function() {
+      expect(result()).toBe(promise);
+    });
+
+    it('should login via the drupalClient', function() {
+      expect(drupalClient.login).toHaveBeenCalledWith(
+        'u',
+        'p',
+        jasmine.any(Function),
+        jasmine.any(Function),
+        jasmine.objectContaining({'Content-Type': 'application/json'}));
+      });
+
+    it('should have called the resolve function of the deferred', function() {
+      expect(deferred.resolve).toHaveBeenCalledWith(userData);
+    });
+
+    it('should trigger the digest cycle', function() {
+      expect($rootScope.$apply).toHaveBeenCalled();
+    });
+
+    it('should set the uid', function() {
+      expect(gentleSite.uid).toBe(10);
+    });
   });
 
-  it('should trigger the digest cycle', function() {
-    expect($rootScope.$apply).toHaveBeenCalled();
+  describe('when failed to log in', function(){
+    var e = null;
+
+    beforeEach(function() {
+      e = {};
+      drupalClient.login = function(username, password, success, error) {
+        error(e);
+      }
+
+      spyOn(drupalClient, 'login').and.callThrough();
+
+      gentleSite = new GentleSite(drupalClient, $q, $rootScope);
+
+      result = gentleSite.login('u', 'p');
+    });
+
+    it('should return a Promise', function() {
+      expect(result()).toBe(promise);
+    });
+
+    it('should tried to login via the drupalClient', function() {
+      expect(drupalClient.login).toHaveBeenCalledWith(
+        'u',
+        'p',
+        jasmine.any(Function),
+        jasmine.any(Function),
+        jasmine.objectContaining({'Content-Type': 'application/json'}));
+      });
+
+    it('should have called the reject function of the deferred', function() {
+      expect(deferred.reject).toHaveBeenCalledWith(e);
+    });
+
+    it('should trigger the digest cycle', function() {
+      expect($rootScope.$apply).toHaveBeenCalled();
+    });
+
+    it('should clear the uid', function() {
+      expect(gentleSite.uid).toBe(undefined);
+    });
   });
 
-  it('should clear the uid', function() {
-    expect(gentleSite.uid).toBe(undefined);
-  });
-});
-
-describe('A GentleSite, when logging out', function() {
-  var gentleSite, drupalClient, $q, result, promise, userData, deferred, $rootScope = null;
-
-  beforeEach(function() {
-    drupalClient = {
-      systemConnect: function() {},
-      logout: function(success, error) {
+  describe('when logging out', function() {
+    beforeEach(function() {
+      drupalClient.logout = function(success, error) {
         success();
-      }
-    };
-    spyOn(drupalClient, 'logout').and.callThrough();
+      };
 
-    deferred = {
-      promise: function() {
-        promise = {};
-        return promise;
-      },
-      resolve: function() {}
-    };
-    spyOn(deferred, 'resolve');
+      spyOn(drupalClient, 'logout').and.callThrough();
 
-    $q = {
-      defer: function() {
-        return deferred;
-      }
-    };
+      gentleSite = new GentleSite(drupalClient, $q, $rootScope);
+      gentleSite.uid = 10;
 
-    $rootScope = {
-      $apply: function() {}
-    };
-    spyOn($rootScope, '$apply');
-
-    gentleSite = new GentleSite(drupalClient, $q, $rootScope);
-    gentleSite.uid = 10;
-
-    result = gentleSite.logout();
-  });
-
-  it('should return a Promise', function() {
-    expect(result()).toBe(promise);
-  });
-
-  it('should logout via the drupalClient', function() {
-    expect(drupalClient.logout).toHaveBeenCalledWith(
-      jasmine.any(Function),
-      jasmine.any(Function),
-      jasmine.objectContaining({'Content-Type': 'application/json'}));
+      result = gentleSite.logout();
     });
 
-  it('should have called the resolve function of the deferred', function() {
-    expect(deferred.resolve).toHaveBeenCalled();
+    it('should return a Promise', function() {
+      expect(result()).toBe(promise);
+    });
+
+    it('should logout via the drupalClient', function() {
+      expect(drupalClient.logout).toHaveBeenCalledWith(
+        jasmine.any(Function),
+        jasmine.any(Function),
+        jasmine.objectContaining({'Content-Type': 'application/json'}));
+      });
+
+    it('should have called the resolve function of the deferred', function() {
+      expect(deferred.resolve).toHaveBeenCalled();
+    });
+
+    it('should trigger the digest cycle', function() {
+      expect($rootScope.$apply).toHaveBeenCalled();
+    });
+
+    it('should clear the uid', function() {
+      expect(gentleSite.uid).toBe(undefined);
+    });
   });
 
-  it('should trigger the digest cycle', function() {
-    expect($rootScope.$apply).toHaveBeenCalled();
-  });
+  describe('when failing to log out', function() {
+    var e = null;
 
-  it('should clear the uid', function() {
-    expect(gentleSite.uid).toBe(undefined);
-  });
-});
+    beforeEach(function() {
+      e = {};
 
-describe('A GentleSite, when failing to log out', function() {
-  var gentleSite, drupalClient, $q, result, promise, userData, deferred, $rootScope, e = null;
-
-  beforeEach(function() {
-    drupalClient = {
-      systemConnect: function() {},
-      logout: function(success, error) {
+      drupalClient.logout = function(success, error) {
         e = {};
         error(e);
-      }
-    };
-    spyOn(drupalClient, 'logout').and.callThrough();
+      };
 
-    deferred = {
-      promise: function() {
-        promise = {};
-        return promise;
-      },
-      reject: function() {}
-    };
-    spyOn(deferred, 'reject');
+      spyOn(drupalClient, 'logout').and.callThrough();
 
-    $q = {
-      defer: function() {
-        return deferred;
-      }
-    };
+      gentleSite = new GentleSite(drupalClient, $q, $rootScope);
 
-    $rootScope = {
-      $apply: function() {}
-    };
-    spyOn($rootScope, '$apply');
-
-    gentleSite = new GentleSite(drupalClient, $q, $rootScope);
-
-    result = gentleSite.logout();
-  });
-
-  it('should return a Promise', function() {
-    expect(result()).toBe(promise);
-  });
-
-  it('should tried to logout via the drupalClient', function() {
-    expect(drupalClient.logout).toHaveBeenCalledWith(
-      jasmine.any(Function),
-      jasmine.any(Function),
-      jasmine.objectContaining({'Content-Type': 'application/json'}));
+      result = gentleSite.logout();
     });
 
-  it('should have called the reject function of the deferred', function() {
-    expect(deferred.reject).toHaveBeenCalledWith(e);
-  });
-
-  it('should trigger the digest cycle', function() {
-    expect($rootScope.$apply).toHaveBeenCalled();
-  });
-});
-
-describe('A gentleSite, when getting successfully', function() {
-  var gentleSite, drupalClient, $q, result, promise, serverResult, deferred, $rootScope = null;
-
-  beforeEach(function() {
-    serverResult = {};
-
-    drupalClient = {
-      systemConnect: function() {},
-      makeAuthenticatedRequest: function(options, success, error, headers) {
-        success(serverResult);
-      }
-    };
-    spyOn(drupalClient, 'makeAuthenticatedRequest').and.callThrough();
-
-    deferred = {
-      promise: function() {
-        promise = {};
-        return promise;
-      },
-      resolve: function() {}
-    };
-    spyOn(deferred, 'resolve');
-
-    $q = {
-      defer: function() {
-        return deferred;
-      }
-    };
-
-    $rootScope = {
-      $apply: function() {}
-    };
-    spyOn($rootScope, '$apply');
-
-    gentleSite = new GentleSite(drupalClient, $q, $rootScope);
-
-    result = gentleSite.get('saldo/10');
-  });
-
-  it('should return a Promise', function() {
-    expect(result()).toBe(promise);
-  });
-
-  it('should tried to make an authenticated request via the drupalClient', function() {
-    expect(drupalClient.makeAuthenticatedRequest).toHaveBeenCalledWith(
-      jasmine.objectContaining({
-        'httpMethod': 'GET',
-        'servicePath': 'saldo/10',
-        'contentType': 'application/json'
-      }),
-      jasmine.any(Function),
-      jasmine.any(Function),
-      jasmine.objectContaining({'Content-Type': 'application/json'}));
-  });
-
-  it('should have called the resolve function of the deferred', function() {
-    expect(deferred.resolve).toHaveBeenCalled();
-  });
-
-  it('should trigger the digest cycle', function() {
-    expect($rootScope.$apply).toHaveBeenCalled();
-  });
-});
-
-describe('A gentleSite, when getting fails', function() {
-  var gentleSite, drupalClient, $q, result, promise, deferred, $rootScope, e = null;
-
-  beforeEach(function() {
-    drupalClient = {
-      systemConnect: function() {},
-      makeAuthenticatedRequest: function(options, success, error, headers) {
-        e = {};
-        error(e);
-      }
-    };
-    spyOn(drupalClient, 'makeAuthenticatedRequest').and.callThrough();
-
-    deferred = {
-      promise: function() {
-        promise = {};
-        return promise;
-      },
-      reject: function() {}
-    };
-    spyOn(deferred, 'reject');
-
-    $q = {
-      defer: function() {
-        return deferred;
-      }
-    };
-
-    $rootScope = {
-      $apply: function() {}
-    };
-    spyOn($rootScope, '$apply');
-
-    gentleSite = new GentleSite(drupalClient, $q, $rootScope);
-
-    result = gentleSite.get('saldo/10');
-  });
-
-  it('should return a Promise', function() {
-    expect(result()).toBe(promise);
-  });
-
-  it('should tried to make an authenticated request via the drupalClient', function() {
-    expect(drupalClient.makeAuthenticatedRequest).toHaveBeenCalledWith(
-      jasmine.objectContaining({
-        'httpMethod': 'GET',
-        'servicePath': 'saldo/10',
-        'contentType': 'application/json'
-      }),
-      jasmine.any(Function),
-      jasmine.any(Function),
-      jasmine.objectContaining({'Content-Type': 'application/json'}));
+    it('should return a Promise', function() {
+      expect(result()).toBe(promise);
     });
+
+    it('should tried to logout via the drupalClient', function() {
+      expect(drupalClient.logout).toHaveBeenCalledWith(
+        jasmine.any(Function),
+        jasmine.any(Function),
+        jasmine.objectContaining({'Content-Type': 'application/json'}));
+      });
 
     it('should have called the reject function of the deferred', function() {
       expect(deferred.reject).toHaveBeenCalledWith(e);
@@ -406,3 +219,88 @@ describe('A gentleSite, when getting fails', function() {
       expect($rootScope.$apply).toHaveBeenCalled();
     });
   });
+
+  describe('when getting successfully', function() {
+    var serverResult = null;
+
+    beforeEach(function() {
+      serverResult = {};
+
+      drupalClient.makeAuthenticatedRequest = function(options, success, error, headers) {
+        success(serverResult);
+      };
+
+      spyOn(drupalClient, 'makeAuthenticatedRequest').and.callThrough();
+
+      gentleSite = new GentleSite(drupalClient, $q, $rootScope);
+
+      result = gentleSite.get('saldo/10');
+    });
+
+    it('should return a Promise', function() {
+      expect(result()).toBe(promise);
+    });
+
+    it('should tried to make an authenticated request via the drupalClient', function() {
+      expect(drupalClient.makeAuthenticatedRequest).toHaveBeenCalledWith(
+        jasmine.objectContaining({
+          'httpMethod': 'GET',
+          'servicePath': 'saldo/10',
+          'contentType': 'application/json'
+        }),
+        jasmine.any(Function),
+        jasmine.any(Function),
+        jasmine.objectContaining({'Content-Type': 'application/json'}));
+      });
+
+    it('should have called the resolve function of the deferred', function() {
+      expect(deferred.resolve).toHaveBeenCalled();
+    });
+
+    it('should trigger the digest cycle', function() {
+      expect($rootScope.$apply).toHaveBeenCalled();
+    });
+  });
+
+  describe('when getting fails', function() {
+    var e = null;
+
+    beforeEach(function() {
+      e = {};
+
+      drupalClient.makeAuthenticatedRequest = function(options, success, error, headers) {
+        error(e);
+      }
+
+      spyOn(drupalClient, 'makeAuthenticatedRequest').and.callThrough();
+
+      gentleSite = new GentleSite(drupalClient, $q, $rootScope);
+
+      result = gentleSite.get('saldo/10');
+    });
+
+    it('should return a Promise', function() {
+      expect(result()).toBe(promise);
+    });
+
+    it('should tried to make an authenticated request via the drupalClient', function() {
+      expect(drupalClient.makeAuthenticatedRequest).toHaveBeenCalledWith(
+        jasmine.objectContaining({
+          'httpMethod': 'GET',
+          'servicePath': 'saldo/10',
+          'contentType': 'application/json'
+        }),
+        jasmine.any(Function),
+        jasmine.any(Function),
+        jasmine.objectContaining({'Content-Type': 'application/json'}));
+      });
+
+    it('should have called the reject function of the deferred', function() {
+      expect(deferred.reject).toHaveBeenCalledWith(e);
+    });
+
+    it('should trigger the digest cycle', function() {
+      expect($rootScope.$apply).toHaveBeenCalled();
+    });
+  });
+});
